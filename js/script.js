7a02ba6e2285da845dcd8a87e0ec41f53be016e1
Murat Kilic <muratkilic1978@gmail.com>
2021-10-13 07:11:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* VARIABLES: */
+/* Step 1 - Use document.getElementById to fecth the id named "popup" and store the value in a variable (let) named "popupElem" followed by a semicolon */
+
+
+
+/* EventListener */
+/* Syntax addEventListener: element.addEventListener(event, function) */
+/* Step 2 - add a addEventListener to the variable "popupElem". The addEventListener should work on a click event a call and then call the function hideBox followed by a semicolon*/
+
+
+
+/* FUNCTIONS: */
+
+
+
+/* Step 3 - Create a function named "hideBox()"" { } with open and end curly-brackets */
+/* Step 3.1 - Inside the curly-brackets in the function - do following. Set style on "popupElem" to display = "none" followed by a semicolon */
+
+
+
